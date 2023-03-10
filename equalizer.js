9f4ca825439e058de536a7507197ce229d369403
AsmrForProgramming <zpnfilter23@gmail.com>
2023-03-11 02:26:27 +0330

    music player project

---
+++ equalizer.js
@@ -0,0 +1,19 @@
+
+
+let lines =document.querySelectorAll(".vertical")
+
+function setEqualizer(){
+    for (let i=0;i<lines.length; i+=1 ){
+        let line=lines[i];
+        line.style.animation=`equalizer ${Math.random()* (3-0.3) + 0.3}s ease infinite`;
+        line.style.animationDirection='alternate-reverse' 
+    }
+}
+
+function stopEqualizer(){
+    for (let i=0;i<lines.length; i+=1  ){
+        let line=lines[i];
+        line.style='height: 0px';
+        
+    }
+} 
